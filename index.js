7bf4e8b0ae33c85504e612479f9ee62a8bd49e9b
Adam Miribyan <john@doe.org>
2022-01-17 19:16:41 +0100

    Render wallet NFTs

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const getWeb3 = async () => {
-  return new Promise(async (resolve, reject) => {
-    const web3 = new Web3(window.ethereum)
-
-    try {
-      await window.ethereum.request({ method: "eth_requestAccounts" })
-      resolve(web3)
-    } catch (error) {
-      reject(error)
-    }
-  })
-}
+import SpacePunksTokenABI from "./space_punks_token.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("connect_button").addEventListener("click", async ({ target }) => {
-    const web3 = await getWeb3()
-    const walletAddress = await web3.eth.requestAccounts()
-    const walletBalanceInWei = await web3.eth.getBalance(walletAddress[0])
-    const walletBalanceInEth = Math.round(Web3.utils.fromWei(walletBalanceInWei) * 1000) / 1000
+  const web3 = new Web3(window.ethereum)
 
-    target.setAttribute("hidden", "hidden")
+  document.getElementById("load_button").addEventListener("click", async () => {
+    const contract = new web3.eth.Contract(SpacePunksTokenABI, "0x45DB714f24f5A313569c41683047f1d49e78Ba07")
+    const walletAddress = document.getElementById("wallet_address").value
+    contract.defaultAccount = walletAddress
+    const spacePunksBalance = await contract.methods.balanceOf(walletAddress).call()
+    
+    document.getElementById("nfts").innerHTML = ""
 
-    document.getElementById("wallet_address").innerText = walletAddress
-    document.getElementById("wallet_balance").innerText = walletBalanceInEth
-    document.getElementById("wallet_info").removeAttribute("hidden")
+    for(let i = 0; i < spacePunksBalance; i++) {
+      const tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, i).call()
+
+      let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call()
+
+      if (tokenMetadataURI.startsWith("ipfs://")) {
+        tokenMetadataURI = `https://ipfs.io/ipfs/${tokenMetadataURI.split("ipfs://")[1]}`
+      }
+
+      const tokenMetadata = await fetch(tokenMetadataURI).then((response) => response.json())
+
+      const spacePunkTokenElement = document.getElementById("nft_template").content.cloneNode(true)
+      spacePunkTokenElement.querySelector("h1").innerText = tokenMetadata["name"]
+      spacePunkTokenElement.querySelector("a").href = `https://opensea.io/assets/0x45db714f24f5a313569c41683047f1d49e78ba07/${tokenId}`
+      spacePunkTokenElement.querySelector("img").src = tokenMetadata["image"]
+      spacePunkTokenElement.querySelector("img").alt = tokenMetadata["description"]
+
+      document.getElementById("nfts").append(spacePunkTokenElement)
+    }
   })
 })
